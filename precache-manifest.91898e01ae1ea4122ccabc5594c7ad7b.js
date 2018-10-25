d084bc5464b7a431fd1373103cbd3959f48aff6f
chukwudike <bryan.uwanaka@gmail.com>
2018-10-25 03:36:18 +0100

    Updates

---
+++ precache-manifest.91898e01ae1ea4122ccabc5594c7ad7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a66aa00960d94caa42",
+    "url": "/lyrics-app/static/js/runtime~main.61a66aa0.js"
+  },
+  {
+    "revision": "f0d59052fcec6fe0d0e0",
+    "url": "/lyrics-app/static/js/main.f0d59052.chunk.js"
+  },
+  {
+    "revision": "d2e839be0b7574ff3922",
+    "url": "/lyrics-app/static/js/1.d2e839be.chunk.js"
+  },
+  {
+    "revision": "f0d59052fcec6fe0d0e0",
+    "url": "/lyrics-app/static/css/main.b1b61eea.chunk.css"
+  },
+  {
+    "revision": "add097b27da220e71b15a0a8cd9686b2",
+    "url": "/lyrics-app/index.html"
+  }
+];
